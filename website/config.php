c6420ffb16ffaf8c01445db2551e1cee6e1feae6
@Marwood_Wolf <cleako@gmail.com>
2018-03-30 21:17:50 -0400

    Replaced
    Quite a lot replaced, primarily the path and name.

---
+++ website/config.php
@@ -0,0 +1,19 @@
+<?php
+
+$db_type = 'mysqli';
+$db_host = 'localhost';
+$db_name = 'wolf_kingdom';
+$db_username = 'wolf';
+$db_password = 'malware';
+$db_prefix = '';
+$p_connect = false;
+
+$cookie_name = 'luna_cookie_4e2448';
+$cookie_domain = '';
+$cookie_path = '/';
+$cookie_secure = 0;
+$cookie_seed = 'd8f57f0faa47aa13';
+
+define('PUN', 1);
+//define('LUNA_DEBUG', 1);
+//define('LUNA_SHOW_QUERIES', 1);
